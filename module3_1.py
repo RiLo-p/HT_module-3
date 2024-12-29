bc118e2c98f5e13b7f65add44f8404dbb5f21986
RiLo-p <wiwer.alemoure@gmail.com>
2024-12-29 22:55:09 +0300

    Add files via upload

---
+++ module3_1.py
@@ -0,0 +1,13 @@
+calls = 0
+def count_calls ():
+    global calls
+    calls += 1
+def string_info (string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains (string, list_to_search):
+    count_calls ()
+    return string.upper() in [i.upper() for i in list_to_search]
+print(string_info('Гиппогриф'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(calls)
